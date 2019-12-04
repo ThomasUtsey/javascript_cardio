38af9fc74967ebad445cc0bbd5d8e612c93a2775
Thomas Utsey <tom@edgegluer.com>
2019-12-03 20:42:09 -0500

    complete session one and session two

--- session1/index_start.js
+++ session1/index_start.js
@@ -3,48 +3,110 @@
 // ex. reverseString('hello') === 'olleh'
 
 function reverseString(str) {
-  return 'Let\'s Start'
-}
-
-
-
-// CHALLENGE 2: VALIDATE A PALINDROME
-// Return true if palindrome and false if not
-// ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
-
-function isPalindrome(str) {}
-
-
-
-// CHALLENGE 3: REVERSE AN INTEGER
-// Return an integer in reverse
-// ex. reverseInt(521) === 125
-
-function reverseInt(int) {}
-
-
-
-// CHALLENGE 4: CAPITALIZE LETTERS
-// Return a string with the first letter of every word capitalized
-// ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
-function capitalizeLetters(str) {}
-
-
-
-// CHALLENGE 5: MAX CHARACTER
-// Return the character that is most common in a string
-// ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str) {}
-
-
-
-// CHALLENGE 6: FIZZBUZZ
-// Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
-
-
-
-// Call Function
-const output = reverseString('hello');
-
-console.log(output);
+  //  return str
+  //   .split('')
+  //   .reverse()
+  //   .join('')
+  return str
+    .split('')
+    .reduce((revStr,char) => revStr = char + revStr,'')
+  }
+  
+  
+  
+  // CHALLENGE 2: VALIDATE A PALINDROME
+  // Return true if palindrome and false if not
+  // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
+  
+  function isPalindrome(str) {
+  return str === str
+  .split('')
+  .reverse()
+  .join('')
+  }
+  
+  
+  // CHALLENGE 3: REVERSE AN INTEGER
+  // Return an integer in reverse
+  // ex. reverseInt(521) === 125
+  
+  function reverseInt(int) {
+    return parseInt(int
+      .toString()
+      .split('')
+      .reverse()
+      .join('')) 
+      * Math.sign(int)
+    }
+  
+  
+  
+  // CHALLENGE 4: CAPITALIZE LETTERS
+  // Return a string with the first letter of every word capitalized
+  // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
+  function capitalizeLetters(str) {
+    // let newString = ''
+    //   for(word of str.split(' ')){
+    //     newString =newString +
+    //      ' ' 
+    //      + word
+    //         .substring(0,1)
+    //         .toUpperCase()
+    //      + word
+    //         .substring(1)
+    //   }    
+    //   return newString
+  
+    return str.replace(/\b[a-z 0-9]/gi, char => char.toUpperCase())
+      }
+  
+  
+  
+  
+  
+  // CHALLENGE 5: MAX CHARACTER
+  // Return the character that is most common in a string
+  // ex. maxCharacter('javascript') == 'a'
+  function maxCharacter(str) {
+    const chrMap = []
+    let maxNum = 0
+    let maxChr = ''
+    for(chr of str){
+      chrMap[chr]
+      ?chrMap[chr]++
+      :chrMap[chr] = 1
+      
+    
+    if(chrMap[chr] > maxNum){
+      maxNum = chrMap[chr]
+      maxChr = chr
+    }
+  }
+    return maxChr
+  }
+  
+  
+  
+  // CHALLENGE 6: FIZZBUZZ
+  // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
+  function fizzBuzz() {
+    const fizzBuzz = []
+   for(i = 0; i<=100; i++){
+     i > 0 && i % 3 === 0 && i % 5 === 0
+      ? fizzBuzz.push(`${i}: FizzBuzz`)
+      : i > 0 && i % 3 === 0
+        ? fizzBuzz.push(`${i}: Fizz`)
+        : i > 0 && i % 5 === 0
+          ? fizzBuzz.push(`${i}:Buzz`)
+          : fizzBuzz.push(i)
+   }
+  
+  return fizzBuzz
+  
+  }
+  
+  // Call Function
+  const output = capitalizeLetters('test thids function');
+  
+  console.log(output);
+  
--- session2/index_start.js
+++ session2/index_start.js
@@ -3,6 +3,14 @@
 // ex. longestWord('Hi there, my name is Brad') === 'there,'
 
 function longestWord(sen) {
+  const longArr = sen
+    .toLowerCase()
+    .match(/[a-z0-9]+/g)
+    .sort((a, b) => b.length - a.length);
+
+  longestWord = longArr.filter(word => word.length >= longArr[0].length);
+
+  return longestWord.length === 1 ? longestWord[0] : longestWord;
   // SOLUTION 1 - Return a single longest word
   // SOLUTION 2 - Return an array and include multiple words if they have the same length
   // SOLUTION 3 - Only return an array if multiple words, otherwise return a string
@@ -13,29 +21,88 @@ function longestWord(sen) {
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6],[7]]
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
 
-function chunkArray(arr, len) {}
+function chunkArray(arr, len) {
+  // const chunk = []
+  // let i = 0
+  // while(i < arr.length){
+  //   chunk
+  //     .push(arr
+  //       .slice(i,len + i))
+  //   i = i+len
+  // }
+  // return chunk
+
+  const chunk = [];
+
+  arr.forEach(e => {
+    const last = chunk[chunk.length - 1];
+    !last || last.length === len ? chunk.push([e]) : last.push(e);
+  });
+  return chunk;
+}
 
 // CHALLENGE 3: FLATTEN ARRAY
 // Take an array of arrays and flatten to a single array
 // ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
 
-function flattenArray(arrays) {}
+function flattenArray(arrays) {
+  // return arrays
+  //   .reduce((a,b)=> a.concat(b),'')
+
+  //------------------------------------------
+
+  // return []
+  // .concat
+  // .apply([],arrays)
+
+  //---------------------------------------
+
+  // return []
+  // .concat
+  // .apply([],arrays)
+
+  return [].concat(...arrays);
+}
 
 // CHALLENGE 4: ANAGRAM
 // Return true if anagram and false if not
 // ex. 'elbow' === 'below'
 // ex. 'Dormitory' === 'dirty room##'
 
-function isAnagram(str1, str2) {}
+isAnagram = (str1, str2) => formatStr(str1) === formatStr(str2);
 
+formatStr = str =>
+  str
+    .replace(/[^\w]/g, "")
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
 // CHALLENGE 5: LETTER CHANGES
 // Change every letter of the string to the one that follows it and capitalize the vowels
 // Z should turn to A
 // ex. 'hello there' === 'Ifmmp UIfsf'
 
-function letterChanges(str) {}
+function letterChanges(str) {
+  let newStr = str
+    .toLowerCase()
+    .replace(/[a-z]/gi, char => {
+    if (char === "z" || char === "Z") {
+      return "a";
+    } else {
+      return String
+        .fromCharCode(char.charCodeAt() + 1);
+    }
+  });
+
+  newStr = newStr.replace(/[aeiou]/g,(char) => char.toUpperCase())
+
+  return newStr;
+}
 
 // Call Function
-const output = longestWord('Hello, my name is Brad');
+
+const array = [[1, 2], [3, 4], [5, 6], [7]];
+const output = letterChanges("wrtwertddsgheert");
 
 console.log(output);
